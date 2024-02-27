ca15e140758dbb4489c0b3ba7ccb88d3ed8d14c9
IaroslavtsevAleksandr <iaroslavtsevaleksandr@gmail.com>
2024-02-27 18:49:32 +0400

    commiting mymath version 1.0.0

--- mymath.go
+++ mymath.go
@@ -26,3 +26,33 @@ func Yn(n int, x float64) float64 {
 func Pow(x, n float64) float64 {
 	return math.Pow(x, n)
 }
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Cos(x float64) float64 {
+	return math.Cos(x)
+}
+
+func Sin(x float64) float64 {
+	return math.Sin(x)
+}
+
+func Asinh(x float64) float64 {
+	return math.Asinh(x)
+}
