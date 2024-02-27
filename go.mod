fc122162e376be7ddbc8a7122a9f68f20f58fe65
windblows123 <iaroslavtsevaleksandr@gmail.com>
2024-02-27 06:54:16 +0400

    1st commit of mymath

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/windblows123/mymath
+
+go 1.19
---
+++ mymath.go
@@ -0,0 +1,28 @@
+package mymath
+
+import "math"
+
+// Sqrt - функция для вычисления квадратного корня из числа
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
+func Pow(x, n float64) float64 {
+	return math.Pow(x, n)
+}
